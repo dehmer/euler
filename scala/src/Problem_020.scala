d5e7ccdba06fbbcc8e89bda86fa094836e522433
dehmer <horst.dehmer@gmail.com>
2012-02-09 19:54:38 +0100

    Problem 34.

--- scala/src/Problem_020.scala
+++ scala/src/Problem_020.scala
@@ -1,6 +1,5 @@
 
 object Problem_020 extends App {
-  def fac(n: BigInt): BigInt = if (n == BigInt(1)) BigInt(1) else n * fac(n - 1)
-
+  import Predef.fac
   println(fac(100).toString().map(c => c - '0').reduce(_ + _))
 }
