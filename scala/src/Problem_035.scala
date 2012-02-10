48b8ef8502428eaf80b2befd358dbe4d29da28c1
dehmer <horst.dehmer@gmail.com>
2012-02-10 09:36:04 +0100

    Problems 36, 37.

--- scala/src/Problem_035.scala
+++ scala/src/Problem_035.scala
@@ -2,26 +2,14 @@
 object Problem_035 extends App {
   import Predef.{primes, timed}
 
-  def binarySearch[A <% Ordered[A]](a: IndexedSeq[A], v: A) = {
-    def recurse(low: Int, high: Int): Option[Int] = (low + high) / 2 match {
-      case _ if high < low => None
-      case mid if a(mid) > v => recurse(low, mid - 1)
-      case mid if a(mid) < v => recurse(mid + 1, high)
-      case mid => Some(mid)
-    }
-    recurse(0, a.size - 1)
-  }
-
   def rotations(n: Long): List[Long] = {
     def rotate(s: String, n: Int) = (s.substring(n) + s.substring (0, n)).toLong
     val s = n.toString
-
     (1 to s.length() - 1).foldLeft(List[Long]())((acc, x) => rotate(s, x) :: acc)
   }
 
   timed("duration: %d ms.") {
-    val ps = primes.takeWhile(_ < 1000000).toArray
-    val result = ps.filter(x => rotations(x).forall(p => binarySearch(ps, p).isDefined)).size
-    assert(55 == result)
+    val ps = primes.takeWhile(_ < 1000000).toSet // hash-based set for faster lookup
+    assert(55 == ps.filter(x => rotations(x).forall(p => ps.contains(p))).size)
   }
 }
