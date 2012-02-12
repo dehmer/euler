479d986b4fe522bed11ebe36f0a3a2387a1108c4
dehmer <horst.dehmer@gmail.com>
2012-02-12 10:24:57 +0100

    reading a file, FP style.

--- ruby/euler_022.rb
+++ ruby/euler_022.rb
@@ -1,14 +1,6 @@
-start = Time.new # debug only
-
-data = []
-File.open("names.txt", "r") do |file|
-    while (name = file.gets(','))
-        data << name.delete('",')
-    end
-end
-
+data = File.open("names.txt", "r").each(',').collect { |name| name.delete('",') }
 result = data.sort.zip((2..(data.length+2)).to_a).inject(0) do |total, (name, index)|
   total + name.bytes.inject(0) { |sum, b| sum + b - ?A + 1 } * index
 end
 
-print result, ", ", Time.new - start, " secs\n" # 871198282
+print result, "\n"
