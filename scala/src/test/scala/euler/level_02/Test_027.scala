aa8560ad418d49b1b1cfd00dc79b86fcc879355e
dehmer <horst.dehmer@gmail.com>
2012-02-13 22:29:39 +0100

    replaced 3-tuple with case class.

--- scala/src/test/scala/euler/level_02/Test_027.scala
+++ scala/src/test/scala/euler/level_02/Test_027.scala
@@ -16,24 +16,21 @@ class Test_027 {
   }
 
   def answer = {
-    def isPrime(n: Long) = (n > 1) && (Primes takeWhile {
-      _ <= math.sqrt(n)
-    } forall {
-      n % _ != 0
-    })
+    case class Answer(s: Int, a: Int, b: Long)
+    def isPrime(n: Long) = (n > 1) && (Primes.takeWhile(_ <= math.sqrt(n)).forall(n % _ != 0))
     def quadratic(a: Int, b: Long) = (n: Long) => n * n + a * n + b
 
-    val answer = (-999 to 999 by 2).foldLeft((0, 0, 0L)) {
+    val answer = (-999 to 999 by 2).foldLeft(Answer(0, 0, 0L)) {
       (acc, x) =>
         val c = Primes.takeWhile(_ < 999).foldLeft(acc) {
-          case (acc, y) =>
+          (acc, y) =>
             val sequence = (0 #:: Naturals).takeWhile(n => isPrime(math.abs(quadratic(x, y)(n)))).size
-            if (sequence > acc._1) (sequence, x, y) else acc
+            if (sequence > acc.s) Answer(sequence, x, y) else acc
         }
 
-        if (c._1 > acc._1) c else acc
+        if (c.s > acc.s) c else acc
     }
 
-    answer._2 * answer._3
+    answer.a * answer.b
   }
 }
